18e6076f65386e9ecb14c7d31628f078e3318f4a
Sumi451 <sharmasumiran12@gmail.com>
2025-06-11 17:46:40 +0530

    Changes to be committed: 	renamed:    .venv/.gitignore -> .gitignore 	new file:   test/digit1.png 	new file:   test/digit2.png 	new file:   test/digit3.png 	new file:   test/digit4.png 	new file:   train.py

---
+++ train.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import keras
+import tensorflow as tf
+
+#directly import dataset module from keras
+mnist= tf.keras.datasets.mnist 
+#x is pixel data and y is classification data
+(x_train, y_train), (x_test,y_test) = mnist.load_data()
+ 
+#normalize the data
+x_train= tf.keras.utils.normalize(x_train,axis=1)
+x_test= tf.keras.utils.normalize(x_test, axis=1)
+
+model=tf.keras.models.Sequential()
+#now flatten the image
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+#add dense layers
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='softmax'))
+
+#compile the model afterward`s`
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+#now fit/train the model
+model.fit(x_train,y_train, epochs=10)
+
+model.save('handwritten.keras')
